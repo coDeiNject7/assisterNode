6a259535f4bf940dd77a44e8a4c453379967e0d7
pav <pavangowda0717@gmail.com>
2025-08-25 21:30:26 +0530

    hard coded the .env valuse in the db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
+  host: "assister-mysql-1.mysql.database.azure.com",
+  user: "azureadmin",
+  password: "FillClock07",
+  database: "assister",
+  port: 3306,
   ssl: {
     rejectUnauthorized: false
   },
